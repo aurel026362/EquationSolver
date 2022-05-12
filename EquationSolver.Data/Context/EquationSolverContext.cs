09f7584beef5bcf2c3c594cc57edc3f78553fc95
Aurel Starciuc <starciuc.aurel@outlook.com>
2022-05-12 15:20:14 +0300

    Add project files.

---
+++ EquationSolver.Data/Context/EquationSolverContext.cs
@@ -0,0 +1,22 @@
+﻿using EquationSolver.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquationSolver.Data.Context
+{
+    public class EquationSolverContext : DbContext
+    {
+        public EquationSolverContext(DbContextOptions<EquationSolverContext> dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-999TVV1\SQLEXPRESSBLOODY;Database=EquationSolverDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+        public DbSet<EquationData> EquationData { get; set; }
+    }
+}
